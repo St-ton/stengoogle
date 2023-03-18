1f6f8d2870ca3728b3100edae6acc0c39dcc6bc0
Stanislav Tonkich <st.tonkikh@gmail.com>
2023-03-19 00:48:23 +0100

    update 1903

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -14,7 +14,8 @@ const Search = () => {
   }, [debouncedValue]);
 
   return (
-    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+    // <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+    <div className="relative sm:-mt-10 mt-3">
       <input
         value={text}
         type="text"
